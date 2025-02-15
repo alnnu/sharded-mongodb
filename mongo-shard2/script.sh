0e772ab2f691da99cc306eaf0e697dd1216bcfd7
Jose Roberto <joserobertomi@outlook.com>
2025-02-15 19:23:21 -0300

    add: scripts de mongosh

---
+++ mongo-shard2/script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+docker stop $(docker ps -aq)
+docker network create mongo
+
+docker compose up -d
+
+CONTAINER_ID=$(docker-compose ps -q)
+echo "ID do Container: $CONTAINER_ID"
+
+# Esperar até que o Mongo esteja pronto
+echo "Aguardando MongoDB iniciar..."
+
+# Esse loop tenta conectar no Mongo até dar certo
+until docker exec $CONTAINER_ID mongosh --eval "db.runCommand({ ping: 1 })"; do
+    echo "MongoDB não está pronto ainda... tentando novamente em 2 segundos..."
+    sleep 2
+done
+
+echo "MongoDB pronto! Conectando..."
+
+docker exec -it $CONTAINER_ID mongosh --eval rs.initiate()
